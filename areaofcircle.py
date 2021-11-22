c07037fa518cc55b63f0ae40fefb86e7dfd840a2
Roshnitasya <94867939+Roshnitasya@users.noreply.github.com>
2021-11-23 01:25:11 +0530

    Add files via upload

---
+++ areaofcircle.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 00:30:35 2021
+
+@author: Roshni
+"""
+
+
+pi = 3.14
+r = float(input("Enter the radius of the circle: "))
+area = pi * r * r
+print("area of circle of radius",r,"is",area);
+
+
+
+
+x = input("Input the Filename: ")
+f = x.split(".")
+print ("The extension of the file is : " , f) 
