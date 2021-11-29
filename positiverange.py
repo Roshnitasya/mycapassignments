07730d6123ed42061dd7d5a84935cc97fe313803
Roshnitasya <94867939+Roshnitasya@users.noreply.github.com>
2021-11-29 13:57:39 +0530

    Add files via upload

---
+++ positiverange.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 12:53:49 2021
+
+@author: Roshni Tasya 
+"""
+
+list1=[12, -7, 5, 64, -14]
+
+for num in list1:
+    if num>=0:
+        print(num)
+        
+
+list2= [12, 14, -95, 3]
+
+for num in list2:
+    if num>=0:
+        print(num)
