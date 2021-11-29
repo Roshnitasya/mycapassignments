9f3f39f55d0d400318a035c968fc33d27d525038
Roshnitasya <94867939+Roshnitasya@users.noreply.github.com>
2021-11-29 15:59:52 +0530

    Add files via upload

---
+++ mississippi.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 15:47:14 2021
+
+@author: Roshni Tasya 
+"""
+
+myDict={"M":0,"I":0,"S":0,"P":0}
+word = "MISSISSIPPI"
+for i in word :
+    if i=="M":
+        myDict["M"] = myDict["M"]+1
+    elif i=="I":
+        myDict["I"] = myDict["I"]+1
+    elif i=="S":
+        myDict["S"] = myDict["S"]+1 
+    elif i=="P":
+        myDict["P"] = myDict["P"]+1
+        
+print(myDict)        
