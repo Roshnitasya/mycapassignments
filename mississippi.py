bf3dabe13d04e5d025a71e82fd711d34bf5a38db
Roshnitasya <94867939+Roshnitasya@users.noreply.github.com>
2021-12-05 10:42:14 +0530

    Update mississippi.py

--- mississippi.py
+++ mississippi.py
@@ -5,16 +5,14 @@ Created on Mon Nov 29 15:47:14 2021
 @author: Roshni Tasya 
 """
 
-myDict={"I":0,"S":0,"P":0,"M":0}
-word = "MISSISSIPPI"
-for i in word :
-    if i=="M":
-        myDict["M"] = myDict["M"]+1
-    elif i=="I":
-        myDict["I"] = myDict["I"]+1
-    elif i=="S":
-        myDict["S"] = myDict["S"]+1 
-    elif i=="P":
-        myDict["P"] = myDict["P"]+1
-        
-print(myDict)        
+def word(str):
+    dict = {}
+    for n in str:
+        num = dict.keys()
+        if n in num:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+print(word('mississippi'))
+
